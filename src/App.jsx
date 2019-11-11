cffec9fefc6426d7cc4aa5f750f49955e8bff7a0
Gerardo May <cgme.may@gmail.com>
2019-11-11 07:58:03 -0600

    init menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import logo from './logo.svg';
+import 'normalize.css';
 import './App.css';
+
 import store from './store';
+import Menu from './components/Menu';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-              Edit
-            {' '}
-            <code>src/App.js</code>
-            {' '}
-and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              Learn React
-          </a>
-        </header>
-      </div>
+      <main className="App">
+        <Menu />
+        <Wrapper>
+          <h1>Estamos en el wrapper</h1>
+        </Wrapper>
+      </main>
     </Provider>
 
   );
--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux';
-import reducer from './reducers';
+import reducers from './reducers';
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
-  reducer, /* preloadedState, */
+  reducers, /* preloadedState, */
   +window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 /* eslint-enable */
